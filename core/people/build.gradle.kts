62327d7637bd57f49191ea60422667f9c4fb196b
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 15:48:48 +0300

    Stage 4: add people module Gradle config

---
+++ core/people/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins { alias(libs.plugins.kotlin.jvm) }
+kotlin { jvmToolchain(17) }
+dependencies {
+    implementation(project(":core:civilization"))
+    implementation(project(":core:simulation"))
+    testImplementation(libs.junit)
+}
