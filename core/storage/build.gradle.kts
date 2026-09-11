9254dab23d84a131a27def5196066235b1acedc0
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 16:21:19 +0300

    Stage 5: persist economy state

--- core/storage/build.gradle.kts
+++ core/storage/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation(project(":core:civilization"))
     implementation(project(":core:history"))
     implementation(project(":core:people"))
+    implementation(project(":core:economy"))
     implementation(project(":core:simulation"))
     testImplementation(libs.junit)
 }
