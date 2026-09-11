34fe7479ccad208df4447b31248a2fc3d6957e34
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 20:16:14 +0300

    Respect Android safe drawing insets in playable shell

--- app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
@@ -3,10 +3,18 @@ package com.sendmefile77.chronosphere
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { ChronospherePlayableApp() }
+        setContent {
+            Box(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+                ChronospherePlayableApp()
+            }
+        }
     }
 }
