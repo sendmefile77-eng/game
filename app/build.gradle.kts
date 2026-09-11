de6af6f2ebb0e3d82e3831b923942ab6e9a6b95f
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 21:31:12 +0300

    Declare Compose Foundation directly in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.foundation:foundation")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     testImplementation(libs.junit)
