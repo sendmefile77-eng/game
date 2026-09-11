0b474373cf12688ba7b096f2e3eb2763a753aa24
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 22:46:52 +0300

    Switch Android entry point to map-first game UI

--- app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Box(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
-                ChronospherePlayableApp()
+                ChronosphereGameApp()
             }
         }
     }
