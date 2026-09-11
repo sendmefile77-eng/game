f6a350905531a55bdb7ddc23e044349dbfb630d5
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-11 20:14:04 +0300

    Add JVM coverage for playable simulation runner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    testImplementation(libs.junit)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
