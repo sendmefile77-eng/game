be191ba9c574b6406820107aef142316a029fbfa
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-12 20:22:36 +0300

    Match Android system bars to Chronosphere theme

--- app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sendmefile77.chronosphere
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,11 @@ import com.sendmefile77.chronosphere.llm.TellamaSettingsStore
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("DEPRECATION")
+        run {
+            window.statusBarColor = Color.rgb(8, 13, 18)
+            window.navigationBarColor = Color.rgb(8, 13, 18)
+        }
         TellamaRuntime.configureApiKey(TellamaSettingsStore.load(this))
         setContent {
             Box(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
