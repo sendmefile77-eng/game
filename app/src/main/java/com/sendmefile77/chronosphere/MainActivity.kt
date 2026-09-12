9eab6863641d241315971da33dd5b8fa69db9df6
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-12 18:45:02 +0300

    Load Tellama key on app startup

--- app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
+import com.sendmefile77.chronosphere.llm.TellamaRuntime
+import com.sendmefile77.chronosphere.llm.TellamaSettingsStore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TellamaRuntime.configureApiKey(TellamaSettingsStore.load(this))
         setContent {
             Box(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
                 ChronosphereGameApp()
