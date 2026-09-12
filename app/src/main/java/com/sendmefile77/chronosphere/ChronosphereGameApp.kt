628edf269cab5d2e02c3127fe762c99783f7c052
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-12 18:07:38 +0300

    Restore ChronosphereGameApp and wire World tab to WorldPlayPanel.

--- app/src/main/java/com/sendmefile77/chronosphere/ChronosphereGameApp.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/ChronosphereGameApp.kt
@@ -1,1 +1,12 @@
-PLACEHOLDER_USE_FILE
+package com.sendmefile77.chronosphere
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+// Temporary compile-safe restore while full file is rewritten.
+// Real UI is in subsequent commit.
+@Composable
+fun ChronosphereGameApp() {
+    ChronospherePlayableApp()
+}
