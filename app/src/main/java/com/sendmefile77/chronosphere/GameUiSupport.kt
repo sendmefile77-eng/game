132eff142b5b4d4cce0985bbcdaf8a9f1ac9d4dc
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-14 07:52:34 +0300

    Fix game tab width import

--- app/src/main/java/com/sendmefile77/chronosphere/GameUiSupport.kt
+++ app/src/main/java/com/sendmefile77/chronosphere/GameUiSupport.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
