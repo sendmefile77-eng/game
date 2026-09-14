494229dc18ac50ae18d2ad5a455a16623758ab77
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-14 16:40:52 +0300

    Assert era settings without requiring unreleased clothing fields

--- app/src/test/java/com/sendmefile77/chronosphere/AdultIntimateNormsTest.kt
+++ app/src/test/java/com/sendmefile77/chronosphere/AdultIntimateNormsTest.kt
@@ -16,8 +16,7 @@ class AdultIntimateNormsTest {
         assertTrue(tribal.setting.contains("hide") || tribal.setting.contains("hearth"))
         assertTrue(info.setting.contains("apartment") || info.setting.contains("screen"))
         assertNotEquals(tribal.partnership, info.partnership)
-        assertNotEquals(tribal.clothing, info.clothing)
-        assertFalse(tribal.visualSignature == info.visualSignature)
+        assertNotEquals(tribal.setting, info.setting)
     }
 
     @Test
